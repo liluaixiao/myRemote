b33352a49a5f24eeb7f37fde779c488339c2dc6d
LiLu <lilu@chelun.com>
2017-05-31 17:39:11 +0800

    modify index

--- react/ScrollViewProject/index.android.js
+++ react/ScrollViewProject/index.android.js
@@ -9,24 +9,152 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  ScrollView,
+  TouchableOpacity,
+  View,
+  Image,
+  WebView,
+  TextInput
 } from 'react-native';
 
-export default class ScrollViewProject extends Component {
+class UselessTextInput extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <TextInput editable = {true}
+                  maxlength = {5}
+                  {...this.props} />
+    )
+  }
+}
+
+export default class ScrollViewProject extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text : 'write a sentence say hello',
+      changBg : true
+    }
+  }
+  render() {
+    return (
+      <ScrollView contentContainerStyle={styles.contentContainer} horizontal={false} >
+        <View>
+          <TextInput value={this.state.text}
+                    onChangeText = {(text) => this.setState({text})} 
+                    {...this.props}/>
+          <TouchableOpacity>
+            <Text>{this.state.text}</Text>
+          </TouchableOpacity>
+          <Text>{this.state.onBlur}</Text>
+        </View>
+        <View style={{
+                       borderBottomColor: '#000000',
+                       borderBottomWidth: 5 }} style={this.state.changBg ? styles.inputBg : styles.inputBgred}>
+          <UselessTextInput multiline = {false}
+                            numberOfLines = {4}
+                            onChangeText={(text) => this.setState({text})}
+                            value={this.state.text}
+                            underlineColorAndroid="transparent"
+                            autoFocus={false}
+                            selectTextOnFocus={true}
+                            secureTextEntry={true}
+                            onBlur={() => this.setState({changBg: !this.state.changBg})}
+                            onFocus={() => this.setState({changBg: !this.state.changBg})} />
+        </View>
+        <WebView
+            ref="webview"
+            automaticallyAdjustContentInsets={false}
+            style={styles.webView}
+            javaScriptEnabled={true}
+            domStorageEnabled={true}
+            decelerationRate="normal"
+            startInLoadingState={true}
+          />
+        <View style={styles.container}>
+          <Text style={styles.welcome}>
+            Welcome to React Native! 我进来了 哈哈hhhh
+          </Text>
+          <Text style={styles.instructions}>
+            To get started, edit index.android.js
+          </Text>
+          <Text style={styles.instructions}>
+            Double tap R on your keyboard to reload,{'\n'}
+            Shake or press menu button for dev menu
+          </Text>
+        </View>
+        <View style={styles.container}>
+          <Text style={styles.welcome}>
+            Welcome to React Native! 我进来了 哈哈hhhh
+          </Text>
+          <Text style={styles.instructions}>
+            To get started, edit index.android.js
+          </Text>
+          <Text style={styles.instructions}>
+            Double tap R on your keyboard to reload,{'\n'}
+            Shake or press menu button for dev menu
+          </Text>
+        </View>
+        <View style={styles.container}>
+          <Text style={styles.welcome}>
+            Welcome to React Native! 我进来了 哈哈hhhh
+          </Text>
+          <Text style={styles.instructions}>
+            To get started, edit index.android.js
+          </Text>
+          <Text style={styles.instructions}>
+            Double tap R on your keyboard to reload,{'\n'}
+            Shake or press menu button for dev menu
+          </Text>
+        </View>
+        <View style={styles.container}>
+          <Text style={styles.welcome}>
+            Welcome to React Native! 我进来了 哈哈hhhh
+          </Text>
+          <Text style={styles.instructions}>
+            To get started, edit index.android.js
+          </Text>
+          <Text style={styles.instructions}>
+            Double tap R on your keyboard to reload,{'\n'}
+            Shake or press menu button for dev menu
+          </Text>
+        </View>
+        <View style={styles.container}>
+          <Text style={styles.welcome}>
+            Welcome to React Native! 我进来了 哈哈hhhh
+          </Text>
+          <Text style={styles.instructions}>
+            To get started, edit index.android.js
+          </Text>
+          <Text style={styles.instructions}>
+            Double tap R on your keyboard to reload,{'\n'}
+            Shake or press menu button for dev menu
+          </Text>
+        </View>
+        <View style={styles.container}>
+          <Text style={styles.welcome}>
+            Welcome to React Native! 我进来了 哈哈hhhh
+          </Text>
+          <Text style={styles.instructions}>
+            To get started, edit index.android.js
+          </Text>
+          <Text style={styles.instructions}>
+            Double tap R on your keyboard to reload,{'\n'}
+            Shake or press menu button for dev menu
+          </Text>
+        </View>
+        <View style={styles.container}>
+          <Text style={styles.welcome}>
+            Welcome to React Native! 我进来了 哈哈hhhh
+          </Text>
+          <Text style={styles.instructions}>
+            To get started, edit index.android.js
+          </Text>
+          <Text style={styles.instructions}>
+            Double tap R on your keyboard to reload,{'\n'}
+            Shake or press menu button for dev menu
+          </Text>
+        </View>
+      </ScrollView>
     );
   }
 }
@@ -48,6 +176,19 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  contentContainer: {
+    paddingVertical: 20
+  },
+  webView: {
+    backgroundColor: 'lightblue',
+    height: 350,
+  },
+  inputBg: {
+    backgroundColor: 'lightgreen'
+  },
+  inputBgred: {
+    backgroundColor: 'red'
+  }
 });
 
 AppRegistry.registerComponent('ScrollViewProject', () => ScrollViewProject);
